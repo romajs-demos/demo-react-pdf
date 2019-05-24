720d9dadfbeb813e33a2dd921ae4dd2ccacb33c5
ohager <oliver.hager@dextra-sw.com>
2019-05-24 11:17:59 -0300

    added static doc selector

--- src/components/ReactPdfViewer.js
+++ src/components/ReactPdfViewer.js
@@ -1,12 +1,11 @@
 import './ReactPdfViewer.css';
 import {Document, Page, pdfjs} from 'react-pdf';
-import {range} from '../utils';
 import React, {Fragment} from 'react';
 import weblog from 'webpack-log';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
-const log = weblog({name: 'react-pdf-viewer'})
+const log = weblog({name: 'react-pdf-viewer'});
 
 class PdfPage extends React.Component {
 
@@ -25,19 +24,25 @@ class PdfPage extends React.Component {
 
   nextPage = () => {
     const {currentPage} = this.state;
-    this.setState({isRendering: true, currentPage: Math.min(currentPage + 1, this.props.numPages)}, () => {
-    })
+
+    const newPage  = Math.min(currentPage + 1, this.props.numPages);
+    if(newPage !== currentPage) {
+      this.setState({isRendering: true, currentPage: newPage})
+    }
   }
 
   prevPage = () => {
     const {currentPage} = this.state;
-    this.setState({isRendering: true, currentPage: Math.max(currentPage - 1, 1)})
+    const newPage  = Math.max(currentPage - 1, 1);
+    if(newPage !== currentPage) {
+      this.setState({isRendering: true, currentPage: newPage})
+    }
   }
 
-  onRenderSuccess = (e) => {
+  onRenderSuccess = () => {
     this.setState({isRendering: false});
 
-    if(!this.pageElement || !this.renderingElement) return;
+    if (!this.pageElement || !this.renderingElement) return;
 
     this.renderingElement.style.height = `${this.pageElement.offsetHeight}px`;
     this.renderingElement.style.width = `${this.pageElement.offsetWidth}px`;
@@ -52,7 +57,8 @@ class PdfPage extends React.Component {
 
         <div className='react-pdf-viewer-document__body'>
           <div className='arrow-left' onClick={this.prevPage}/>
-          <div ref={ ref => this.renderingElement = ref} className={`react-pdf-viewer-page--rendering ${!this.state.isRendering && 'hidden'}`}/>
+          <div ref={ref => this.renderingElement = ref}
+               className={`react-pdf-viewer-page--rendering ${!this.state.isRendering && 'hidden'}`}/>
           <Page
             inputRef={ref => this.pageElement = ref}
             className={`react-pdf-viewer-page ${this.state.isRendering && 'hidden'}`}
@@ -70,47 +76,65 @@ class PdfPage extends React.Component {
 }
 
 
-class ReactPdfViewer extends React.Component {
-  state = {
-    documentLoaded: false,
-    numPages: 0,
-    currentPage: 1,
-    reload: true
+const PdfPath = '/assets/pdf';
+
+const PdfOptions = [
+  {
+    url: `${PdfPath}/report_light_text_only.pdf`,
+    name: 'Simple Text Document (2 pages)'
+  },
+  {
+    url: `${PdfPath}/report_real_world.pdf`,
+    name: 'Test Exam (1 page)'
+  },
+  {
+    url: `${PdfPath}/report_medium.pdf`,
+    name: 'Images and Texts (9 pages)'
+  },
+  {
+    url: `${PdfPath}/report_huge.pdf`,
+    name: 'Huge Document (98 pages)'
   }
+];
+
+class ReactPdfViewer extends React.Component {
 
   constructor() {
     super()
-    this._doc = '/assets/pdf/report_large_2.pdf'
+
+    const initialDoc = PdfOptions[0].url
+    this._doc = initialDoc
+    this.state = {
+      numPages: 0,
+      selectedDoc: initialDoc
+    }
   }
 
-  onDocumentLoadSuccess = ({numPages, fingerprint}) => {
+  onDocumentLoadSuccess = ({numPages}) => {
     this.setState({numPages});
   }
 
-  updateUrl = (event) => {
-    const {value: url} = event.target
-    log.info('url:', url)
-    this.setState({url, documentLoaded: false, reload: false})
-  }
-
-  fetchUrl = () => {
-    log.info('fetchUrl')
-    this.setState({numPages: 0, documentLoaded: false, reload: true})
+  handleDocSelect = ({target}) => {
+    const pdfUrl = target.value;
+    this._doc = pdfUrl;
+    this.setState({selectedDoc: pdfUrl})
   }
 
   render() {
-    const {numPages, url, pdfData} = this.state
+    const {numPages} = this.state;
 
     log.info('#render, url:', this._doc);
 
     return (
       <div className='react-pdf-viewer'>
         <form className='react-pdf-viewer-form'>
-          <label htmlFor='url'>URL:</label>
-          <input type="text" name='url' value={url} onChange={this.updateUrl}/>
-          <input type="button" name='fetch-url' value='Fetch' onClick={this.fetchUrl}/>
+          <label htmlFor='url'>Document: </label>
+          <select value={this.state.selectedDoc} onChange={this.handleDocSelect}>
+            {
+              PdfOptions.map(doc => <option value={doc.url}>{doc.name}</option>)
+            }
+          </select>
         </form>
-        <p className='react-pdf-viewer-info'>Please verify if URL input accept CORS (Access-Control-Allow-Origin: *)</p>
         <Document
           className='react-pdf-viewer-document'
           file={this._doc}
